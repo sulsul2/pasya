73bcaaf9a658aff55c346d62c44a69cbe904d8ba
sulsul2 <18221110@std.stei.itb.ac.id>
2024-03-19 14:03:53 +0700

    add theme

---
+++ lib/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = const Color(0xFFF9FAFB);
+Color blackColor = const Color(0xFF000000);
+Color yellowColor = const Color(0xFFFFC85D);
+Color greyColor = const Color(0xFFC6C8D2);
+Color blueColor = const Color(0xFF2C4F91);
+Color secondaryYellowColor = const Color(0xFFEDD098);
+
+double paddingHorizontal = 20;
+double defaultMargin = 12;
+double defaultRadius = 12;
+
+TextStyle whiteText = GoogleFonts.lato(color: whiteColor);
+TextStyle blackText = GoogleFonts.lato(color: blackColor);
+TextStyle yelllowText = GoogleFonts.lato(color: yellowColor);
+TextStyle greyText = GoogleFonts.lato(color: greyColor);
+TextStyle blueText = GoogleFonts.lato(color: blueColor);
+TextStyle secondaryYellowText = GoogleFonts.lato(color: secondaryYellowColor);
